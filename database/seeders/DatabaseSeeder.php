5fd0d612635aca634ce33dfbba1ff0374a4deeef
Moh Adib Syambudi <syambudiadib@gmail.com>
2023-10-03 14:44:41 +0700

    add player table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Club;
+use App\Models\Player;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -23,5 +24,6 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         Club::factory(25)->create();
+        Player::factory(50)->create();
     }
 }
