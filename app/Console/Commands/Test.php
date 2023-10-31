674da8c2ee9a26702f20537d7cd5a993d56c0b51
Dewangga Dika Darmawan <dewangga3d@gmail.com>
2023-10-31 14:17:48 +0700

    feat: coba scheduling

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test untuk mencoba Scheduling';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return $this->info($this->description);
+    }
+}
