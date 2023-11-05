1a093a2868134db0eec13fc0199256c6cc7b6a85
Dewangga Dika Darmawan <dewangga3d@gmail.com>
2023-11-05 11:46:54 +0700

    feat: add tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,9 @@
         </style>
     </head>
     <body class="antialiased">
+        <div class="container bg-white text-black">
+            <h1>Test Tailwind</h1>
+        </div>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
