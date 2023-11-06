8f9e6661fbe497fcc8663d99d82d1aae17adf04c
Dewangga Dika Darmawan <dewangga3d@gmail.com>
2023-11-06 12:52:55 +0700

    feat: add feature test

---
+++ tests/Feature/databaseTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Club;
+use Database\Seeders\DatabaseSeeder;
+
+class databaseTest extends TestCase
+{
+    public function test_seeder()
+    {
+        // Cek jumlah data yang ada di database
+        // $this->assertDatabaseCount('clubs', 25);
+
+        // Cek jumlah data yang di-seed dan yang nanti ada di database
+        // $this->seed(DatabaseSeeder::class);
+        // $this->assertDatabaseCount('clubs', 25);
+
+        // Cek jumlah data dari club yang berasal dari Surabaya
+        $this->assertDatabaseHas('clubs', ['city' => 'Surabaya']);
+    }
+}
